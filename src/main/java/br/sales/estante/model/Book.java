e12a7c8f43aa34fa713a71f5f017fc7984a93b0d
Felipe Sales <felipe.ferreira.sales@gmail.com>
2021-12-16 23:20:03 -0300

    adicionando Lombok

--- src/main/java/br/sales/estante/model/Book.java
+++ src/main/java/br/sales/estante/model/Book.java
@@ -1,8 +1,11 @@
 package br.sales.estante.model;
 
+import lombok.Data;
+import lombok.Getter;
+
 import java.util.UUID;
 
-
+@Data
 public class Book {
 
     private UUID id;
@@ -13,11 +16,4 @@ public class Book {
 //    private CreativeTeam creativeTeam;
 //    private Country country;
 
-    public UUID getId() {
-        return this.id;
-    }
-
-    public String getTitle() {
-        return this.title;
-    }
 }
