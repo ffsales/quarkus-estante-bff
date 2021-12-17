8e8615cf5f988569937076c544a60536c6b71a09
Felipe Sales <felipe.ferreira.sales@gmail.com>
2021-12-17 00:01:35 -0300

    Iniciando documentação da api

--- src/main/java/br/sales/estante/api/BooksController.java
+++ src/main/java/br/sales/estante/api/BooksController.java
@@ -1,6 +1,10 @@
 package br.sales.estante.api;
 
 import br.sales.estante.domain.BookSerice;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -11,12 +15,18 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/v1/books")
+@Tag(name = "/v1/books", description = "API de livros")
 public class BooksController {
 
     @Inject
     private BookSerice bookSerice;
 
     @GET
+    @Operation(description = "API que retorna uma lista de todos os livros")
+    @APIResponses(value = {
+            @APIResponse(description = "Retorna 200 para sucesso", responseCode = "200"),
+            @APIResponse(description = "retorna 404 se não encontrar nenhum", responseCode = "404")
+    })
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllBook() {
 
--- src/main/java/br/sales/estante/model/Book.java
+++ src/main/java/br/sales/estante/model/Book.java
@@ -2,13 +2,17 @@ package br.sales.estante.model;
 
 import lombok.Data;
 import lombok.Getter;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.util.UUID;
 
 @Data
+@Schema(description = "Objeto que representa o livro")
 public class Book {
 
+    @Schema(description = "Identificador do livro")
     private UUID id;
+    @Schema(description = "Título do livro")
     private String title;
 //    private Genre genre;
 //    private Publishing publishing;
